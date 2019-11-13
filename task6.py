fb2125a2210816b185f9aee66f4f403872d6d43c
pineapple79 <fun.goul.109@gmail.com>
2019-11-13 08:06:16 +0600

    Task1-6

---
+++ task6.py
@@ -0,0 +1,17 @@
+def bigger_guy(one,two):
+    if one > two:
+        return one
+    else:
+        return two
+
+def test_bigger_guy():
+    assert bigger_guy(1, 2) == 2
+    assert bigger_guy(10, 20) == 20
+    assert bigger_guy(20, 10) == 20
+
+    assert bigger_guy(10, 10) == 10
+    assert bigger_guy(2, 1) == 2
+    assert bigger_guy('a', 'b') == 'b' # 'b' is greater than 'a'
+    print("YOUR CODE IS CORRECT!")
+
+test_bigger_guy()
